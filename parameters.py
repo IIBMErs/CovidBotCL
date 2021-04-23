913aa706d494d5453a3594fc833b647b0f8584dd
CarlosValleA <cgvalle@uc.cl>
2021-04-23 01:09:32 -0400

    se creo el archivo parametros que contendra los paths a los archivos

---
+++ parameters.py
@@ -0,0 +1,13 @@
+
+
+# Path a archivos .csv necesarios para obtener la información en datos
+
+# Se optive de producto 1 Archivo "Covid-19.csv"
+PATH_CASOS_INCREMENTALES = "../Datos-COVID19/output/producto1/Covid-19.csv"
+
+# Se obtiene de producto 25 archivo "CasosActualesPorComuna"
+PATH_CASOS_DIARIOS = "../Datos-COVID19/output/producto25/CasosActualesPorComuna.csv"
+
+# Obtener lista de Comunas, se obtiene de producto 1, archivo "Covid-19.csv"
+PATH_LISTA_COMUNAS = "../Datos-COVID19/output/producto1/Covid-19.csv"
+
