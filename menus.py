95d51e987c18556f1d2f2f246e25c501912fa014
CarlosValleA <cgvalle@uc.cl>
2021-05-01 18:14:21 -0400

    se creo el metodo registro en la clase Menu

--- menus.py
+++ menus.py
@@ -3,16 +3,25 @@ from telegram import InlineKeyboardButton
 
 class Menu:
 
-
     @property
     def principal(self):
         # Elementos en el menu princiapl y sus callbacks
-        info_menu = {"Registrarme" : "registrarme",
+        info_menu = {"Registrarme" : "menu_registro",
                     "Notificaciones" : "notificaciones",
                     "Datos" : "datos",
                     "Salir" : "salir"
         } 
 
+        return self.generate_menu(info_menu)
+
+    @property
+    def registro(self):
+        # Elementos en el menu de registro y sus callbacks
+        info_menu = {
+            "Agregar Comuna": "ingresarComuna",
+            "Eliminar Comuna": "eliminarComuna",
+            "Volver": "volver"
+        }
         return self.generate_menu(info_menu)
 
     def generate_menu(self, info_menu):
