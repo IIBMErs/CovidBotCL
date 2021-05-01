fd64967c38eb314fb2bca195245176128e241d3a
CarlosValleA <cgvalle@uc.cl>
2021-05-01 18:40:15 -0400

    se agrego respuestas.py que hara del backend del bot

---
+++ respuestas.py
@@ -0,0 +1,46 @@
+from covidsqlbase  import CovidDataBase
+
+
+class Respuesta:
+
+    def __init__(self):
+        self.covid_data_base = CovidDataBase
+
+
+    
+
+    def registro_response(self, user_id):
+        # Comprobar que el usuario este en la base
+        if self.user_in_base(user_id):
+            # si esta registrado comprobar comunas
+            with self.covid_data_base() as base:
+                comunas = base.getUserComunas(user_id)
+
+                if comunas == []: # Si no hay comunas
+                    text = "Usted no ha registrado su comuna, por favor seleccióne Agregar Comuna para registrar una\. *Puede repetir este proceso para agregar varias\.*"
+
+                else:
+                    # si tiene comunas registradas
+                    temp = ""
+                    for comuna in comunas:
+                        temp = temp + "*" + comuna[0] + "*" + ","
+                    texto = "Usted se encuentra registrado en\: " + temp[:-2] +"\." + "\nPara agregar otra comuna seleccióne Agregar Comuna"
+        else:
+            text = "Usted no ha registrado su comuna, por favor seleccióne Agregar Comuna para registrar una\. *Puede repetir este proceso para agregar varias\.*"
+
+        return text
+
+
+
+
+    def user_in_base(self, user_id):
+        with self.covid_data_base() as base:
+            if (user_id,) in base.getUsers():
+                return True      
+            else:
+                return False
+
+
+
+
+
