02ae36c9bebc8e8c8fb5996b00ef177d57da0ac2
CarlosValleA <cgvalle@uc.cl>
2021-05-01 22:40:21 -0400

    se agregaron los menus de datos y notificaciones a la clase Menus

--- menus.py
+++ menus.py
@@ -1,5 +1,6 @@
 from telegram import InlineKeyboardButton
-
+from covidsqlbase import CovidDataBase
+from parameters import INFO_DATOS
 
 class Menu:
 
@@ -34,6 +35,27 @@ class Menu:
         return keyboard
 
 
+    def create_keyboard_datos(self, user_id=-1, incluir_enroller=False):
+        with CovidDataBase() as base:
+            user_data_suscription = base.getUserDatos(user_id) 
+            user_data_suscription  =[dato[0] for dato in user_data_suscription]
+            
+            info_menu = {}
+            for info in INFO_DATOS:
+                if info[1] in user_data_suscription and incluir_enroller:
+                    info_menu[info[0] + "  \U00002705"] = info[1]
+                else:
+                    info_menu[info[0]] = info[1]
+
+        return info_menu
+
+    def datos(self, user_id):
+        info_menu = self.create_keyboard_datos(user_id, incluir_enrolled=False)
+        return self.generate_menu(info_menu)
+
+    def notificaciones(self, user_id):
+        info_menu = self.create_keyboard_datos(user_id, incluir_enroller=True)
+        return self.generate_menu(info_menu)
 
 
 if __name__ == "__main__":
@@ -41,4 +63,6 @@ if __name__ == "__main__":
     os.system('clear')
     
     menu = Menu()
+
+    menu.create_keyboard_datos()
 
