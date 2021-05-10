31c4a2c89fc4ef261895270d0e0d1c10207bc19a
CarlosValleA <cgvalle@uc.cl>
2021-05-10 17:15:10 -0400

    se agrego algo de paso a paso que no funcono

--- generar_plots.py
+++ generar_plots.py
@@ -1,20 +1,30 @@
-
 import numpy as np
 import pandas as pd
-from datetime import datetime
 from matplotlib import pyplot as plt
 import matplotlib
 import time
 from tqdm import tqdm
 from abc import ABC, abstractmethod
-from parameters import PATH_CASOS_DIARIOS, PATH_CASOS_INCREMENTALES
+import parameters as p
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from datetime import datetime
+from fases import Fase
+import locale
+import json
+locale.setlocale(locale.LC_TIME, '')
+
 
 class Data(ABC):
 
-    def __init__(self,nombre_dato, ruta_archivo):
+    def __init__(self,nombre_dato, identificador, ruta_archivo):
         self.data = pd.read_csv(ruta_archivo)
-        self.weeks = 8  # Cantidad de semanas para hacer los datos
+        self.weeks = 32  # Cantidad de semanas para hacer los datos
+        self.identificador = identificador
         self.nombre_dato = nombre_dato
+        self.fase = Fase()
+
+        self.leer_paso_a_paso()
 
     @property
     def dates(self):
@@ -33,29 +43,122 @@ class Data(ABC):
 
         return wanted_dates
 
+
     @property
     def comunas(self):
         comunas = self.data['Comuna'].values
         comunas = [c for c in comunas if "Desconocido" not in c and "Total" not in c]
         return comunas
 
-    def plot(self, comuna, test=False):
-        x,y = self.puntos(comuna)
+    def leer_paso_a_paso(self):
+        with open(p.PATH_PASO_A_PASO) as json_file:
+            self.paso_a_paso = json.load(json_file)
 
-        plt.plot_date(x,y,linestyle='-')
-        plt.xticks(rotation=90)
-        plt.title(f'Casos Diarios en {comuna}, {self.weeks} ultimas semanas')
+    
+    def info_fase(self, comuna):
+        fase = self.fase.fase_de_comuna(comuna)
+        lista_info = []
+        texto =  self.paso_a_paso[str(fase)]
 
-        if test:
-            plt.show()
+        diff_pos_y = 0
+        for t in texto:
+            dic = dict(
+                    text=t,
+                    showarrow=False,
+                    xref="paper",
+                    yref="paper",
+                    x=-0.17,
+                    y=0.95 + diff_pos_y ,
+                    font= 
+                        dict(family="Arial", size=18, color= "white")
+                    )
+            diff_pos_y += -0.07
+            lista_info.append(dic)
+        return lista_info
 
-        if not test:
-            plt.savefig(f"images/{self.nombre_dato}-{comuna}-weeks{self.weeks}.png")
-            plt.close()
-            print(1)
+
+
+    def plot(self, comuna, save=True):
+        x, y = self.puntos(comuna) 
+        y2 = y[-1]
+        #Init Fig
+        fig = make_subplots( rows=2, cols=1, 
+            specs=[[{"type": "scatter", "rowspan": 1, "colspan":1}], 
+            [{"type": "scatter", "rowspan": 1, "colspan":1}]]
+        )
+
+        #FIG LINE
+        fig.add_trace(
+            go.Scatter(x=x, y=y,
+                marker=dict(
+                    size=8,
+                    cmax=4,
+                    cmin=0,
+                    color='red'
+                )
+                ),
+            row=2, col=1)
+
+
+        #fig.add_annotation(x=x[-1], y=y[-1],
+        #    text="Text annotation with arrow",
+        #    showarrow=True,
+        #    arrowhead=1)
+
+        annotations =[
+                dict(
+                    text="Fuente: MINCIENCIA",
+                    showarrow=False,
+                    xref="paper",
+                    yref="paper",
+                    x=1.1,
+                    y=-0.14,
+                    font_size=15),
+            
+                dict(
+                    text=f"Comuna en fase {self.fase.fase_de_comuna(comuna)}",
+                    showarrow=False,
+                    xref="paper",
+                    yref="paper",
+                    x=-0.17,
+                    y=1.05,
+                    font= 
+                        dict(family="Arial", size=18, color= "white")
+                    ),
+                dict(
+                    text=f"Casos",
+                    showarrow=False,
+                    xref="paper",
+                    yref="paper",
+                    x=-0.18,
+                    y=0.12,
+                    textangle=-90,
+                    font= 
+                        dict(family="Arial", size=21, color= "white")
+                    )
+        ] 
+          
+        # append info
+        for info in self.info_fase(comuna):
+            print(info)
+            annotations.append(info)
+
+        #STYLE
+        fig.update_layout(
+            template="plotly_dark",
+            title=f"{self.nombre_dato}: {comuna}",
+            margin=dict(r=5, t=25, b=40, l=60),
+            width=350,
+            height=350,
+            annotations= annotations )
+
+
+        fig.show()
+
+
 
     def test_plot(self):
-        self.plot('Macul', test=True)
+        self.plot('Macul', save=False)
 
     def generar_plots(self):
         for comuna in tqdm(self.comunas):
@@ -64,8 +167,21 @@ class Data(ABC):
     @abstractmethod
     def puntos(self, comuna):
         comuna_data = self.data[self.data["Comuna"] == comuna]
-        x = self.fechas_ultimas_semanas()
-        y = comuna_data[x].values[0]
+        fechas = self.fechas_ultimas_semanas()
+        casos = comuna_data[fechas].values[0]
+
+        x = []
+        y = []
+        for i in range(len(fechas) - 1):
+            fecha_inicial = datetime.strptime(fechas[i], "%Y-%m-%d")
+            fecha_final = datetime.strptime(fechas[i + 1], "%Y-%m-%d")
+
+            # Dias entre fechas
+            dias = (fecha_final - fecha_inicial).days
+            x.append(fechas[i + 1])
+            y.append(casos[i + 1] / dias)
+            
+        #return fechas, casos
         return (x, y)
 
 
@@ -73,8 +189,8 @@ class Data(ABC):
 
 class CasosIncrementales(Data):
 
-    def __init__(self, nombre_data, ruta_archivo):
-        super().__init__(nombre_data, ruta_archivo)
+    def __init__(self, nombre_data, identificador, ruta_archivo):
+        super().__init__(nombre_data, identificador, ruta_archivo)
 
     def puntos(self, comuna):
         return super().puntos(comuna)
@@ -82,25 +198,33 @@ class CasosIncrementales(Data):
 
 class CasosDiarios(Data):
     
-    def __init__(self, nombre_data, ruta_archivo):
-        super().__init__(nombre_data, ruta_archivo)
+    def __init__(self, nombre_data, identificador, ruta_archivo):
+        super().__init__(nombre_data, identificador, ruta_archivo)
 
     def puntos(self, comuna):
         return super().puntos(comuna)
     
 
+class CasosActivos(Data):
+
+    def __init__(self, nombre_data, identificador, ruta_archivo):
+        super().__init__(nombre_data, identificador, ruta_archivo)
+
+    def puntos(self, comuna):
+        return super().puntos(comuna)
 
 def generar_todos_los_plots():
     lista_datos = [
-        CasosDiarios("casosDiarios", PATH_CASOS_DIARIOS),
-        CasosIncrementales("casosIncrementales", PATH_CASOS_INCREMENTALES)
+        CasosDiarios("casosDiarios", p.PATH_CASOS_DIARIOS),
+        CasosIncrementales("casosIncrementales", p.PATH_CASOS_INCREMENTALES)
     ]
 
     for dato in lista_datos:
         dato.generar_plots()
 
 if __name__ == "__main__":
-    from parameters import PATH_CASOS_DIARIOS, PATH_CASOS_INCREMENTALES
-    test = CasosIncrementales("casosDiarios", PATH_CASOS_INCREMENTALES)
-
-    generar_todos_los_plots()
+    import os
+    os.system('clear')
+    test = CasosActivos("Casos Activos", "casosActivos", p.PATH_CASOS_ACTIVOS)
+    #test.puntos("Macul")
+    test.plot("Chimbarongo")
